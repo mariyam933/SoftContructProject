1b24348532fcebcc9651456cb75508af6dc96857
Ayesha Ejaz Gill <agill.bese21seecs@seecs.edu.pk>
2023-12-29 00:11:30 +0500

    main specs commit

--- SuperMarketManagementSystem/src/main.java
+++ SuperMarketManagementSystem/src/main.java
@@ -1,15 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+/**
+ * The Main class serves as the entry point for the Grocery Management System application.
+ * It contains the main method responsible for initializing and displaying the main frame of the application.
  */
 
 public class main {
 
     /**
-     * @param args the command line arguments
+     * The main method that starts the execution of the Grocery Management System application.
+     *
+     * @param args The command line arguments passed to the application (not used in this implementation).
      */
     public static void main(String[] args) {
+
+        // Create an instance of the MainFrame to start the application
         MainFrame obj = new MainFrame();
+
+        // Make the main frame visible
         obj.setVisible(true);
 
     }
