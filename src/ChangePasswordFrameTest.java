385617ad0f90167d256773c3c27c479ea2f5c470
Minahil Shakoor <minahilshakoor8@gmail.com>
2023-12-29 04:32:46 +0500

    Change Password Frame test cases specifications

---
+++ src/ChangePasswordFrameTest.java
@@ -0,0 +1,48 @@
+/*
+ * Testing strategy for ChangePasswordFrame class
+ *
+ * 1. Test Change Password with Correct Old Password:
+   - Partition the old password correctness: correct, incorrect
+   - Set the correct old password, provide a new password, and change the password.
+   - Verify that the password is updated only when the old password is correct.
+
+ * 2. Test Change Password with Incorrect Old Password:
+   - Similar to the first test but with an incorrect old password.
+   - Verify that the password remains unchanged when the old password is incorrect.
+
+ * 3. Test Change Password with Empty Old Password:
+   - Similar to the first test but with an empty old password.
+   - Verify that the password remains unchanged when the old password is empty.
+
+// */
+
+public class ChangePasswordTest {
+    // covers oldPassField.getText().equals(cs.getPassword()),
+    // oldPassField.getText().equals(cs.getPassword()) is true,
+    // oldPassField.getText().equals(cs.getPassword()) is false
+
+    // newPassField.getText().length() = 0,
+    // newPassField.getText().length() > 0
+    public void changePasswordWithCorrectOldPassword() throws Exception {
+    }
+
+    // covers oldPassField.getText().equals(cs.getPassword()),
+    // oldPassField.getText().equals(cs.getPassword()) is true,
+    // oldPassField.getText().equals(cs.getPassword()) is false
+
+    // newPassField.getText().length() = 0,
+    // newPassField.getText().length() > 0
+    public void changePasswordWithIncorrectOldPassword() throws Exception {
+    }
+
+    // covers oldPassField.getText().equals(cs.getPassword()),
+    // oldPassField.getText().equals(cs.getPassword()) is true,
+    // oldPassField.getText().equals(cs.getPassword()) is false
+
+    // oldPassField.getText().length() = 0
+
+    // newPassField.getText().length() = 0,
+    // newPassField.getText().length() > 0
+    public void changePasswordWithEmptyOldPassword() throws Exception {
+    }
+}
