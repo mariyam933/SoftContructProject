268c9b7d4936f4b9b41d7fb6dc1b6d91b2e025fc
Minahil Shakoor <minahilshakoor8@gmail.com>
2023-12-29 04:30:43 +0500

    User information Frame test cases specifications

---
+++ src/UserInfoFrameTest.java
@@ -0,0 +1,29 @@
+/*
+ * Testing strategy for UserInfoFrame class
+ *
+ * 1. Test Frame Initialization:
+   - Create an instance of UserInfoFrame with user information.
+   - Verify that the frame initializes correctly with the provided user information.
+
+ * 2. Test Back Button Action:
+   - Simulate a button click on the Back button.
+   - Verify that the UserInfoFrame closes.
+
+
+ */
+
+public class UserInfoFrameTest {
+    // Asserts that the UserInfoFrame closes when back button is pressed
+    public void testBackButtonFunctionality() {
+    }
+
+    /*
+     * Assert that the displayed name matches the customer's name.
+     * Assert that the displayed phone number matches the customer's phone number.
+     * Assert that the displayed address matches the customer's address.
+     * Assert that the displayed gender matches the customer's gender.
+     */
+    public void testUserInformationDisplay() {
+    }
+
+}
